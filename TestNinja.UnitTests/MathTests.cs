ccbb1b7cc7148afab29785225251b6f7e3c64e2e
VoN <sudo.von.contact@gmail.com>
2022-01-06 22:51:53 -0600

    feat: Add decorator pattern

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,35 +6,38 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        // TearDown
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            int result = math.Add(1, 2);
+            int result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            int result = math.Max(2, 1);
+            int result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondrgumentIsGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            int result = math.Max(1, 2);
+            int result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-            int result = math.Max(1, 1);
+            int result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
