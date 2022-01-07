ac1b58611be24d459a366bdbb886cb9becf29fd8
VoN <sudo.von.contact@gmail.com>
2022-01-07 12:21:08 -0600

    feat: Add testing void methods

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
