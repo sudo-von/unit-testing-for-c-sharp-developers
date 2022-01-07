ddb577fc6b46c70aa90edf178d3b73bf450b4427
VoN <sudo.von.contact@gmail.com>
2022-01-07 11:53:35 -0600

    feat: Add testing return types of methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IDIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+            // NotFound or one of its derivatives
+            // Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IDIsNotZero_ReturnOk()
+        {
+
+        }
+    }
+}
