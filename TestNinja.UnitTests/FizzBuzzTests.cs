9703ed4bd16862d3014b6a2e7101a1390bd937ec
VoN <sudo.von.contact@gmail.com>
2022-01-07 15:12:21 -0600

    feat: Add FizzBuzz tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_WhenParamIsDivisibleByThree_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenParamIsDivisibleByFive_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenParamIsDivisibleByThreeAndFive_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenParamIsNotDivisibleByAnyCondition_ReturnSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(1);
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
