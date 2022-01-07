411486852c90a1b20678e9a922cd66823203b84f
VoN <sudo.von.contact@gmail.com>
2022-01-06 22:46:33 -0600

    Add black box tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+            int result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+            int result = math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondrgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+            int result = math.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+            int result = math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
