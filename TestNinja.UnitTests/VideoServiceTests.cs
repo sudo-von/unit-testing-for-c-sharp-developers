3bd774ed2c7d2c1a1805fd1873271bb55b447499
VoN <sudo.von.contact@gmail.com>
2022-01-09 02:03:25 -0600

    feat: Add video service tests using dependency injection

---
+++ TestNinja.UnitTests/VideoServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TextFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var fileReader = new MockFileReader();
+            var service = new VideoService(fileReader);
+            var result = service.ReadVideoTitle();
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
